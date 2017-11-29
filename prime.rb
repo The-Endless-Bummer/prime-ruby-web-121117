bafd5f9d4d4c707148c4f02e3bf4c7bf448bb004
Maurice Roy <mautayro@gmail.com>
2017-11-29 00:08:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  end
+  (2..(int - 1)).each do |current|
+    if int % current == 0
+      return false
+    end
+  end
+  true
+end
